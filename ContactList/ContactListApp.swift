84946150ab72c39f9288ee43f1210e604b33a5b6
Vasiliy Poltorak <zloyrusvelt@gmail.com>
2022-06-08 21:12:39 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Василий Полторак on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
